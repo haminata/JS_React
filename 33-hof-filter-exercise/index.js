9937da8134bf609bbea8cf92cc90d2ecabc8ffae
haminata <amtavcc@hotmail.com>
2021-11-23 20:10:53 +0000

    - learn that whe we use filter, takes in a function that does somthing then applies to the filtered  element

--- 33-hof-filter-exercise/index.js
+++ 33-hof-filter-exercise/index.js
@@ -4,6 +4,12 @@
  * create a function {noStrings} that takes an array
  * as a parameter and return an array but without strings
  */
+function checkAdult(num) {
+    return typeof num !== "string";
+}
+const noStrings = (arParam) => {
+  return arParam.filter(checkAdult);
+}
 
 /**
  * Exercise 2
@@ -13,6 +19,13 @@
  * every other type
  */
 
+const justStrings = (arParam) => {
+    return arParam.filter((num)=>{
+        return typeof num == "string"
+    })
+
+}
+
 /**
  * Exercise 3
  *
@@ -20,6 +33,13 @@
  * an array as a parameter and return an array of strings
  * that are longer than 5 characters
  */
+
+const justStringsLongerThan5 = (arPar) => {
+   return arPar.filter((num)=>{
+        return typeof num =="string" && num.length >5;
+    })
+
+}
 
 /**
  * Exercise 4
@@ -29,12 +49,25 @@
  * that are more than 13
  */
 
+const numbersMoreThan13 = (arPar) => {
+    return arPar.filter((num)=>{
+        return Number.isInteger(num) && num > 13;
+    });
+
+}
+
 /**
  * Exercise 5
  *
  * create a function {objectsOnly} that takes
  * an array as a parameter and return an array of objects
  */
+
+const objectsOnly = (arPar) => {
+  return arPar.filter((num)=>{
+      return typeof num == 'object';
+  })
+}
 
 /**
  * Exercise 6
@@ -43,3 +76,9 @@
  * an array as a parameter and return an array of truthy values.
  * IMPORTANT: You still need to keep number 0.
  */
+
+const truthyOnly = (arPar) => {
+  return arPar.filter((num)=>{
+      return num || num === 0 ;
+  })
+}
