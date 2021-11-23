0a0d942877b440246b260a74c7fbc597898c137e
haminata <amtavcc@hotmail.com>
2021-11-23 12:55:12 +0000

    - learn that whe we use map, takes in a function that does somthing then applies it on each element

--- 32-hof-map-exercise/index.js
+++ 32-hof-map-exercise/index.js
@@ -10,6 +10,12 @@
  * a new array.
  */
 
+const convertToPositive = (arNum) => {
+    return arNum.map((num) =>{
+            return Math.abs(num);
+    });
+}
+
 /**
  * Exercise 2
  *
@@ -23,12 +29,25 @@
  * ]) will return ['Alex', 'John']
  */
 
+const getAllNames = (arUser) => {
+    return  arUser.map((element)=>{
+        return element.name;
+
+    });
+}
+
 /**
  * Exercise 3
  *
  * create a function {greetAll} that takes array of names as a parameter
  * and return a new array of greetings "Hi, {name}!"
  */
+
+function greetAll(arNames) {
+   return  arNames.map((element)=>{
+       return (`Hi, ${element}!`);
+    })
+}
 
 /**
  * Exercise 4
@@ -37,3 +56,17 @@
  * and multiply all numbers inside array by 5 and return the whole array
  * with multiplied numbers
  */
+
+const  multiply5 = (num) =>{
+
+    if(Number.isInteger(num)){
+        return num * 5;
+    }else {
+        return num;
+    }
+
+}
+
+const multiplyAllNumbers =(arPa)=> {
+    return arPa.map(multiply5);
+}
