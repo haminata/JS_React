8330f32ea2412dc7d5dbfe52f19135d0c8786e9e
haminata <amtavcc@hotmail.com>
2022-03-04 01:36:11 +0000

    playground quiz data

---
+++ playground/quiz/README.md
@@ -0,0 +1,7 @@
+    Quiz project
+
+
+- https://www.uidesigndaily.com/posts/sketch-questionnaire-choice-submit-day-924
+- Questions from object
+- Select answer
+- Show score at the end
---
+++ playground/quiz/script.js
@@ -0,0 +1,40 @@
+const quizData = [
+    {
+        question: 'Who created African Giant album?',
+        a: 'Wizkid',
+        b: 'Davido',
+        c: 'Kiss Daniel',
+        d: 'Burna boy',
+        correct: 'd'
+    }, {
+        question: 'Who is MR Eazi?',
+        a: 'Singer',
+        b: 'dancer',
+        c: 'Construction worker',
+        d: 'window cleaner',
+        correct: 'a'
+    }, {
+        question: 'How many Countries are in Africa?',
+        a: '1',
+        b: '30',
+        c: '54',
+        d: '100',
+        correct: 'c',
+    }, {
+        question: 'How many albums does Wizkid have?',
+        a: '7',
+        b: '4',
+        c: '1',
+        d: '10',
+        correct: 'b'
+    }, {
+        question: 'What`s the most popular song from Tiwa Savage',
+        a: 'Eminado',
+        b: 'Ma lo',
+        c: 'Sombody`s son',
+        d: 'Koroba',
+        correct: 'c'
+    }, {
+
+    }
+]
