816dd1c9bc8338237d4ff63c5187c69881f483e3
haminata <amtavcc@hotmail.com>
2022-03-04 17:57:43 +0000

    playground quiz app - load questions implemented and event listener on submit btn

--- playground/quiz/script.js
+++ playground/quiz/script.js
@@ -37,4 +37,44 @@ const quizData = [
     }, {
 
     }
-]
+];
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+
+/*
+ *Keep track of the current question
+ * @currentQuestion should be initialised at 0 represents the index
+ */
+
+let currentQuiz = 0;
+
+/*
+ * @loadQuiz() loads questions everytime we hit the submit button
+ */
+
+const loadQuiz = () => {
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerHTML = currentQuizData.question
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+};
+
+submitBtn.addEventListener('click', e=>{
+    currentQuiz++;
+    if(currentQuiz < quizData.length - 1 ){
+        loadQuiz();
+    }else {
+        alert("Well done, you've completed the quiz!")
+        currentQuiz = 0;
+        loadQuiz();
+    }
+
+})
